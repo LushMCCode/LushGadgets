ed11bc8bd4ecc60105a0f077ab6badd52fe44c2c
Cameron-Witcher <CameronWitcher1@gmail.com>
2022-08-08 18:35:41 -0500

    Changed branch

--- src/main/java/net/lushmc/gadgets/GadgetPlugin.java
+++ src/main/java/net/lushmc/gadgets/GadgetPlugin.java
@@ -1,5 +1,20 @@
 package net.lushmc.gadgets;
 
-public class GadgetPlugin {
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.lushmc.core.commands.AdminCommands;
+import net.lushmc.core.commands.PlayerCommands;
+import net.lushmc.core.utils.CoreUtils;
+
+public class GadgetPlugin extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+
+		CoreUtils.init(this);
+
+		new AdminCommands(this, "lush");
+		new PlayerCommands(this, "console");
+	}
 
 }
