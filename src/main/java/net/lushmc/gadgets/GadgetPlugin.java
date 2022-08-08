c807666972a6feed358e63d5d56ad414982f89dc
Cameron-Witcher <CameronWitcher1@gmail.com>
2022-08-08 18:47:03 -0500

    move to master

--- src/main/java/net/lushmc/gadgets/GadgetPlugin.java
+++ src/main/java/net/lushmc/gadgets/GadgetPlugin.java
@@ -1,5 +1,20 @@
 package net.lushmc.gadgets;
 
+<<<<<<< Updated upstream
 public class GadgetPlugin {
+=======
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GadgetPlugin extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+
+		CoreUtils.init(this);
+
+		new AdminCommands(this, "lush");
+		new PlayerCommands(this, "console");
+	}
+>>>>>>> Stashed changes
 
 }
