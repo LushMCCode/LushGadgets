976fcc9854e010ba75a830ccebb80489aa5b4aea
Cameron-Witcher <CameronWitcher1@gmail.com>
2022-08-12 10:15:27 -0500

    Update PlayerListener.java

--- src/main/java/net/lushmc/gadgets/listeners/PlayerListener.java
+++ src/main/java/net/lushmc/gadgets/listeners/PlayerListener.java
@@ -9,18 +9,21 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.BlockData;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockExplodeEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.server.TabCompleteEvent;
+import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.lushmc.core.utils.CoreUtils;
 import net.lushmc.gadgets.utils.GadgetUtils;
 import net.lushmc.gadgets.utils.GadgetUtils.GadgetAction;
 import net.lushmc.gadgets.utils.Utils;
@@ -56,15 +59,31 @@ public class PlayerListener implements Listener {
 
 	@EventHandler
 	public void onEntityDamage(EntityDamageByEntityEvent e) {
+		if (e.getEntity() instanceof Player) {
+			// If there are any errors with detecting who killed who, check to see if player
+			// has metadata, remove it, then add new metadata
+			e.getEntity().setMetadata("last_damager", new FixedMetadataValue(Utils.getPlugin(), e.getDamager()));
+			e.getEntity().setMetadata("last_damage_cause", new FixedMetadataValue(Utils.getPlugin(), e.getCause()));
+		}
 		if (e.getCause().equals(DamageCause.ENTITY_EXPLOSION)) {
 			if (!(e.getEntity() instanceof Player))
 				e.setCancelled(true);
 			else {
 				if (e.getDamager().hasMetadata("thrower")
 						&& ((Player) e.getDamager().getMetadata("thrower").get(0).value()).equals(e.getEntity())) {
-					e.setDamage(0.0001);
+					e.setDamage(0.1);
 				}
 			}
+		}
+	}
+
+	@EventHandler
+	public void onPlayerDeath(PlayerDeathEvent e) {
+		if (e.getEntity().hasMetadata("last_damager")) {
+			Entity damager = (Entity) e.getEntity().getMetadata("last_damager").get(0).value();
+			DamageCause cause = (DamageCause) e.getEntity().getMetadata("last_damage_cause").get(0).value();
+			Bukkit.broadcastMessage(CoreUtils.colorize("&a" + damager.getName()));
+			Bukkit.broadcastMessage(CoreUtils.colorize("&c" + cause.toString()));
 		}
 	}
 
